826ee0eeef97e02bc847b5a69e2a7f936144f0ed
Isaac Tan <73921980+isaactan98@users.noreply.github.com>
2022-06-18 14:58:09 +0800

    ---

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app)
+const { v4: uuidv4 } = require("uuid")
+
+const PORT = process.env.PORT || 4000;
+
+const io = require("socket.io")(server, {
+    cors: {
+        origin: "*"
+    }
+})
+
+const { ExpressPeerServer } = require("peer")
+const peerServer = ExpressPeerServer(server, {
+    debug: true
+})
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.use("/peerjs", peerServer)
+
+app.get("/", (req, res) => {
+    res.redirect(`/${uuidv4()}`);
+});
+
+var uuid_value = null;
+app.post('/get_uuid', (req, res) => {
+    uuid_value = uuidv4()
+    res.send(uuid_value);
+})
+
+io.on("connection", (socket) => {
+
+    socket.on("join-room", (roomId, userId, userName) => {
+        socket.join(roomId);
+        socket.to(roomId).emit("user-connected", userId);
+        socket.on("message", (message) => {
+            io.to(roomId).emit("createMessage", message, userName);
+        });
+        socket.on('disconnect', () => {
+            socket.to(roomId).emit('user-disconnected', userId)
+        })
+    });
+})
+
+var i = 0;
+setInterval(() => {
+    i++
+    io.emit("tick", i)
+}, 1000)
+
+server.listen(PORT, () => {
+    console.log("Server is running : " + PORT)
+})
